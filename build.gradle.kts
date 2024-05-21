8b8588e86e03ce84914e3531428183cdb46c7ee8
dora <dora924666990@gmail.com>
2024-05-22 02:39:40 +0800

    添加1.3版本的包

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,7 @@ repositories {
 // See https://github.com/JetBrains/gradle-intellij-plugin/
 intellij {
 //    version.set("231.9392.1.2311.10844163")
-    version.set("203.7717.56.2031.7935034")
+    version.set("213.7172.25.2113.9123335")
     // Android Studio的代号是AI
     type.set("AI")
     plugins.set(listOf("Kotlin", "android"))
